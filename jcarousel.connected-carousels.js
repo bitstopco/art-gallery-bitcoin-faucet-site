2e1dfb491031899bb0cb535dfacd21e21ad0cb56
Peter Carabeo-Nieva <peter@bitstop.co>
2014-12-06 12:50:52 -0500

    Fixes jcarousel connector issue

--- jcarousel.connected-carousels.js
+++ jcarousel.connected-carousels.js
@@ -20,7 +20,7 @@
       var setup = function(data) {
         var stageHTML = '<ul>';
         var thumbnailHTML = '<ul>';
-        
+
         $.each(data.art_pieces, function() {
           stageHTML += '<li><img src="' + baseURL + 'art-pieces-stage/' + this.image_file_name + '" alt="' + this.title + '"></li>';
           thumbnailHTML += '<li><img src="' + baseURL + 'art-pieces-thumbnail/' + this.image_file_name + '" alt="' + this.title + '"></li>';
@@ -36,76 +36,84 @@
         // Reload carousel
         carouselStage.jcarousel('reload');
         carouselNavigation.jcarousel('reload');
-      };
 
-      $.getJSON('art-pieces.json', setup);
+        var connector = function(itemNavigation, carouselStage) {
+          return carouselStage.jcarousel('items').eq(itemNavigation.index());
+        };
 
         // We loop through the items of the navigation carousel and set it up
         // as a control for an item from the stage carousel.
         carouselNavigation.jcarousel('items').each(function() {
-            var item = $(this);
+          var item = $(this);
 
-            // This is where we actually connect to items.
-            var target = connector(item, carouselStage);
+          // This is where we actually connect to items.
 
-            item
-                .on('jcarouselcontrol:active', function() {
-                    carouselNavigation.jcarousel('scrollIntoView', this);
-                    item.addClass('active');
-                })
-                .on('jcarouselcontrol:inactive', function() {
-                    item.removeClass('active');
-                })
-                .jcarouselControl({
-                    target: target,
-                    carousel: carouselStage
-                });
+          var target = connector(item, carouselStage);
+
+          item
+          .on('jcarouselcontrol:active', function() {
+            carouselNavigation.jcarousel('scrollIntoView', this);
+            item.addClass('active');
+          })
+          .on('jcarouselcontrol:inactive', function() {
+            item.removeClass('active');
+          })
+          .jcarouselControl({
+            target: target,
+            carousel: carouselStage
+          });
         });
 
         // Setup controls for the stage carousel
         $('.prev-stage')
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .jcarouselControl({
-                target: '-=1'
-            });
+        .on('jcarouselcontrol:inactive', function() {
+          $(this).addClass('inactive');
+        })
+        .on('jcarouselcontrol:active', function() {
+          $(this).removeClass('inactive');
+        })
+        .jcarouselControl({
+          target: '-=1'
+        });
 
         $('.next-stage')
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .jcarouselControl({
-                target: '+=1'
-            });
+        .on('jcarouselcontrol:inactive', function() {
+          $(this).addClass('inactive');
+        })
+        .on('jcarouselcontrol:active', function() {
+          $(this).removeClass('inactive');
+        })
+        .jcarouselControl({
+          target: '+=1'
+        });
 
         // Setup controls for the navigation carousel
         $('.prev-navigation')
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .jcarouselControl({
-                target: '-=1'
-            });
+        .on('jcarouselcontrol:inactive', function() {
+          $(this).addClass('inactive');
+        })
+        .on('jcarouselcontrol:active', function() {
+          $(this).removeClass('inactive');
+        })
+        .jcarouselControl({
+          target: '-=1'
+        });
 
         $('.next-navigation')
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .jcarouselControl({
-                target: '+=1'
-            });
+        .on('jcarouselcontrol:inactive', function() {
+          $(this).addClass('inactive');
+        })
+        .on('jcarouselcontrol:active', function() {
+          $(this).removeClass('inactive');
+        })
+        .jcarouselControl({
+          target: '+=1'
+        });
+
+
+      };
+
+      $.getJSON('art-pieces.json', setup);
+
     });
 })(jQuery);
